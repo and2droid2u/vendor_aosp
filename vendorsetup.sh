1b717f56f5a89a668b44f14f996b42d0201a12ad
and2droid2u <and2droid2u@gmail.com>
2019-09-03 23:01:38 +0700

    Test commit
    Signed-off-by: and2droid2u <and2droid2u@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,8 @@
-for device in $(python vendor/aosp/tools/get_official_devices.py)
-do
-for var in user userdebug; do
-add_lunch_combo aosp_$device-$var
-done
-done
+#for device in $(python vendor/aosp/tools/get_official_devices.py)
+#do
+#for var in user userdebug; do
+#add_lunch_combo aosp_$device-$var
+#done
+#done
+add_lunch_combo aosp_beryllium-user
+add_lunch_combo aosp_beryllium-userdebug
